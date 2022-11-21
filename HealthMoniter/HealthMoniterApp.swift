1850157d5928e271f5fe4a5285154d48890c27e5
Michael Dougherty <mikedoc1111@gmail.com>
2022-11-21 13:32:35 -0500

    Initial Commit

---
+++ HealthMoniter/HealthMoniterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthMoniterApp.swift
+//  HealthMoniter
+//
+//  Created by  on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthMoniterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
